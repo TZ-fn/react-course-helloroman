f845569187bc100ec7080dfe111f6decd414cf13
Tomek <tomekz@vp.pl>
2020-09-02 13:07:20 +0200

    Updates

---
+++ precache-manifest.9de44a223e1992931671f28625444f96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746334ab3fbd1cc09990fddcbee6335e",
+    "url": "/react-course-helloroman/index.html"
+  },
+  {
+    "revision": "04fd92137d3d31afdb13",
+    "url": "/react-course-helloroman/static/css/main.9d1494d8.chunk.css"
+  },
+  {
+    "revision": "ed65622068ce014c3578",
+    "url": "/react-course-helloroman/static/js/2.1b166961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-helloroman/static/js/2.1b166961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fd92137d3d31afdb13",
+    "url": "/react-course-helloroman/static/js/main.e051e4a8.chunk.js"
+  },
+  {
+    "revision": "a1d0b898a8d90443b835",
+    "url": "/react-course-helloroman/static/js/runtime-main.dd56553d.js"
+  },
+  {
+    "revision": "250c02868e5a837b98e5d95ebf85c18c",
+    "url": "/react-course-helloroman/static/media/danabramov.250c0286.jpg"
+  },
+  {
+    "revision": "3a72cbe0696789492f3395f24616b1dd",
+    "url": "/react-course-helloroman/static/media/kentcdodds.3a72cbe0.jpg"
+  },
+  {
+    "revision": "090afebd465406c24d1366b7d935c264",
+    "url": "/react-course-helloroman/static/media/michaeljackson.090afebd.jpg"
+  },
+  {
+    "revision": "5086cbe8d404204a12006334925c5ec6",
+    "url": "/react-course-helloroman/static/media/ryanflorence.5086cbe8.jpg"
+  }
+]);
