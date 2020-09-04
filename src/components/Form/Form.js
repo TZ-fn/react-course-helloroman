91afd6a3e9c05f5c39e35caa24c7bee35bb35b31
Tomek <tomekz@vp.pl>
2020-09-04 15:00:13 +0200

    Add more form styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,13 +2,33 @@ import React from 'react';
 import './Form.css';
 
 const Form = ({ submitFn }) => (
-  <form onSubmit={submitFn} className='form__wrapper'>
-    <input type="text" placeholder="Name" name="name" className='form__input' />
-    <input type="text" placeholder="Link" name="link" className='form__input' />
-    <input type="text" placeholder="Image" name="image" className='form__input' />
-    <textarea placeholder="Description" name="description" className='form__textarea' />
-    <button type="submit" className='form__button'>Add a new item</button>
-  </form>
+  <div className="form__wrapper">
+    <h2>Add a new twitter account</h2>
+    <form onSubmit={submitFn} className='form__form'>
+
+      <div className="form__form-item">
+        <input type="text" placeholder="Name" name="name" className='form__input' />
+        <label htmlFor="name" className="form__label">Name</label>
+      </div>
+
+      <div className="form__form-item">
+        <input type="text" placeholder="Link" name="link" className='form__input' />
+        <label htmlFor="link" className="form__label">Link</label>
+      </div>
+
+      <div className="form__form-item">
+        <input type="text" placeholder="Image" name="image" className='form__input' />
+        <label htmlFor="image" className="form__label">Image</label>
+      </div>
+
+      <div className="form__form-item">
+        <textarea placeholder="Description" name="description" className='form__textarea' />
+        <label htmlFor="description" className="form__label">Description</label>
+      </div>
+
+      <button type="submit" className='form__button'>Add a new item</button>
+    </form>
+  </div>
 );
 
 export default Form;
