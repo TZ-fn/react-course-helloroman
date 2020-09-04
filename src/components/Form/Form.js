487b0afd5dc37be9cc0805fa22c6a2b037b3face
Tomek <tomekz@vp.pl>
2020-09-04 13:38:51 +0200

    Add form styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Form.css';
 
 const Form = ({ submitFn }) => (
-  <form onSubmit={submitFn}>
-    <input type="text" placeholder="name" name="name" />
-    <input type="text" placeholder="link" name="link" />
-    <input type="text" placeholder="image" name="image" />
-    <textarea placeholder="description" name="description" />
-    <button type="submit">add new item</button>
+  <form onSubmit={submitFn} className='form__wrapper'>
+    <input type="text" placeholder="Name" name="name" className='form__input' />
+    <input type="text" placeholder="Link" name="link" className='form__input' />
+    <input type="text" placeholder="Image" name="image" className='form__input' />
+    <textarea placeholder="Description" name="description" className='form__textarea' />
+    <button type="submit" className='form__button'>Add a new item</button>
   </form>
 );
 
