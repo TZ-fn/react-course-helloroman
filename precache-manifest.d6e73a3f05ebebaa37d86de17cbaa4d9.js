314117a17dcb78a68b68de1a77f71fd80dcabf26
Tomek <tomekz@vp.pl>
2020-09-09 15:08:32 +0200

    Updates

---
+++ precache-manifest.d6e73a3f05ebebaa37d86de17cbaa4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d376f5cab991d8c48e57a581ee71c80a",
+    "url": "/react-course-helloroman/index.html"
+  },
+  {
+    "revision": "e8570bd6ade07b2c589b",
+    "url": "/react-course-helloroman/static/css/main.23073035.chunk.css"
+  },
+  {
+    "revision": "b6cbbc5b122dd70f3a00",
+    "url": "/react-course-helloroman/static/js/2.c762a265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-helloroman/static/js/2.c762a265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8570bd6ade07b2c589b",
+    "url": "/react-course-helloroman/static/js/main.22268e79.chunk.js"
+  },
+  {
+    "revision": "a1d0b898a8d90443b835",
+    "url": "/react-course-helloroman/static/js/runtime-main.dd56553d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-course-helloroman/precache-manifest.0231ea98300027c161325282fb27b2b7.js"
+  "/react-course-helloroman/precache-manifest.d6e73a3f05ebebaa37d86de17cbaa4d9.js"
 );
 
 self.addEventListener('message', (event) => {
