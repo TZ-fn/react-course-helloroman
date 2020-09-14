8bcb902e8967358e8d6ab4dbfdb3de0922dc3959
Tomek <tomekz@vp.pl>
2020-09-14 14:16:28 +0200

    Add a logo and navigation styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import HeaderNavigation from './HeaderNavigation';
+import Button from '../Button/Button'
 import styles from './Header.module.scss';
 import logoImage from '../../assets/images/logo.svg';
 
 const Header = () => (
-  <div>
-    <img scr={logoImage} />
+  <header className={styles.wrapper}>
+    <img src={logoImage} alt='FavNote logo' />
     <HeaderNavigation />
-  </div>
+    <Button>new item</Button>
+  </header>
 );
 
 export default Header;
